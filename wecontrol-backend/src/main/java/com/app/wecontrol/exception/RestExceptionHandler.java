c31beeca26f73d3e1a5876431fd4309e136617d2
Romeu Bessa <emanuelhamachi@hotmail.com>
2024-08-04 22:59:33 -0400

    Ajustes

--- wecontrol-backend/src/main/java/com/app/wecontrol/exception/RestExceptionHandler.java
+++ wecontrol-backend/src/main/java/com/app/wecontrol/exception/RestExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.app.wecontrol.exception;
 
-import com.app.wecontrol.dtos.exception.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
